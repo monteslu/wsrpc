a8ecfd7c01c32d919f2151d4fea09cc693f22e8f
Blaine Bublitz <blaine@iceddev.com>
2014-01-03 12:56:53 -0700

    change UMD wrapper, no more global

--- client.js
+++ client.js
@@ -1,75 +1,69 @@
-(function(factory){
-  /*jshint strict:false */
-  if(typeof define != "undefined"){
-    define(['when'], factory);
-  }else if(typeof module != "undefined"){
-    module.exports = factory(require('when'));
-  }else{
-    Rawr = factory();
-  }
-})(function(when){
-
+(function(define){
   'use strict';
 
-  function Rawr(socket){
-    // Make sure we have a socket with send and on
-    if(!(socket && (socket.on || socket.hasOwnProperty('onmessage')) && socket.send)){
-      return console.warn('Must pass in a socket or an object with a socket property to this cosntructor. ex: {socket : mySocket}');
-    }
+  define(function(require){
 
-    var self = this;
-    var defer = when.defer();
-    this.promise = defer.promise;
-    this.then = defer.promise.then;
-    this.socket = socket;
-    this.methods = {};
+    var when = require('when');
 
-    var _callNum = 0;
-    var _deferreds = {};
-
-    function generateRpc(name, socket){
-      return function(){
-        var deferred = when.defer();
-        _deferreds[++_callNum] = deferred;
-        socket.send(JSON.stringify({
-          method: name,
-          params: Array.prototype.slice.call(arguments),
-          id: _callNum
-        }));
-        return deferred.promise;
-      };
-    }
-
-    // TODO: implement notifications
-
-    function smdHandler(smd){
-      if(!(smd && smd.services)){
-        return defer.reject('Malformed SMD - missing services');
+    function Rawr(socket){
+      // Make sure we have a socket with send and on
+      if(!(socket && (socket.on || socket.hasOwnProperty('onmessage')) && socket.send)){
+        return console.warn('Must pass in a socket or an object with a socket property to this cosntructor. ex: {socket : mySocket}');
       }
 
-      for(var service in smd.services){
-        self.methods[service] = generateRpc(service, self.socket);
+      var self = this;
+      var defer = when.defer();
+      this.promise = defer.promise;
+      this.then = defer.promise.then;
+      this.socket = socket;
+      this.methods = {};
+
+      var _callNum = 0;
+      var _deferreds = {};
+
+      function generateRpc(name, socket){
+        return function(){
+          var deferred = when.defer();
+          _deferreds[++_callNum] = deferred;
+          socket.send(JSON.stringify({
+            method: name,
+            params: Array.prototype.slice.call(arguments),
+            id: _callNum
+          }));
+          return deferred.promise;
+        };
       }
 
-      return defer.resolve(self.methods);
-    }
+      // TODO: implement notifications
 
-    function rpcHandler(rpc){
-      if(!(rpc.id && (rpc.error || rpc.result))){
-        return;
-      }
-
-      var id = rpc.id;
-      if(_deferreds[id]){
-        var defer = _deferreds[id];
-        if(rpc.error){
-          defer.reject(rpc.error);
-        } else {
-          defer.resolve(rpc.result);
+      function smdHandler(smd){
+        if(!(smd && smd.services)){
+          return defer.reject('Malformed SMD - missing services');
         }
-        delete _deferreds[id];
+
+        for(var service in smd.services){
+          self.methods[service] = generateRpc(service, self.socket);
+        }
+
+        return defer.resolve(self.methods);
       }
-    }
+
+      function rpcHandler(rpc){
+        if(!(rpc.id && (rpc.error || rpc.result))){
+          return;
+        }
+
+        var id = rpc.id;
+        if(_deferreds[id]){
+          var defer = _deferreds[id];
+          if(rpc.error){
+            defer.reject(rpc.error);
+          } else {
+            defer.resolve(rpc.result);
+          }
+          delete _deferreds[id];
+        }
+      }
 
       function handleMessage(data){
         if(data.smd){
@@ -95,4 +89,9 @@
 
     return Rawr;
 
-});
+  });
+
+}(
+  typeof define === 'function' && define.amd ? define : function (factory) { module.exports = factory(require); }
+  // Boilerplate for AMD and Node
+));
--- server.js
+++ server.js
@@ -1,76 +1,72 @@
-(function(factory){
-  /*jshint strict:false */
-  if(typeof define != "undefined"){
-    define(['when/function'], factory);
-  }else if(typeof module != "undefined"){
-    module.exports = factory(require('when/function'));
-  }else{
-    Rawr = factory();
-  }
-})(function(whenfn){
-
+(function(define){
   'use strict';
 
-  function Rawr(server, funcs){
-    var sockets = server.sockets ? server.sockets : server;
+  define(function(require){
 
-    this.functions = funcs || Rawr.prototype.functions;
+    var whenfn = require('when/function');
 
-    var self = this;
+    function Rawr(server, funcs){
+      var sockets = server.sockets ? server.sockets : server;
 
-    sockets.on('connection', function (socket) {
+      this.functions = funcs || Rawr.prototype.functions;
 
-      // Generate the SMD and send it on connection
-      for(var func in self.functions){
-        self.smd.services[func] = {};
-      }
-      socket.send(JSON.stringify({smd: self.smd}));
+      var self = this;
 
-      // On rpc call, execute the function
-      socket.on('message', function(data){
-        function onSuccess(result){
-          socket.send(JSON.stringify({
-            result: result,
-            error: null,
-            id: data.id
-          }));
+      sockets.on('connection', function (socket) {
+
+        // Generate the SMD and send it on connection
+        for(var func in self.functions){
+          self.smd.services[func] = {};
         }
+        socket.send(JSON.stringify({smd: self.smd}));
 
-        function onError(error){
-          error = error.message || error;
-          console.log(error);
-          socket.send(JSON.stringify({
-            result: null,
-            error: error,
-            id: data.id
-          }));
-        }
+        // On rpc call, execute the function
+        socket.on('message', function(data){
+          function onSuccess(result){
+            socket.send(JSON.stringify({
+              result: result,
+              error: null,
+              id: data.id
+            }));
+          }
 
-        data = JSON.parse(data);
+          function onError(error){
+            error = error.message || error;
+            socket.send(JSON.stringify({
+              result: null,
+              error: error,
+              id: data.id
+            }));
+          }
 
-        if(self.functions[data.method]){
-          whenfn.apply(self.functions[data.method], data.params).then(onSuccess, onError);
-        } else {
-          onError('method undefined');
-        }
+          data = JSON.parse(data);
+
+          if(self.functions[data.method]){
+            whenfn.apply(self.functions[data.method], data.params).then(onSuccess, onError);
+          } else {
+            onError('method undefined');
+          }
+
+        });
 
       });
+    }
 
-    });
+    Rawr.prototype.functions = {};
 
-    return this;
-  }
+    Rawr.prototype.smd = {
+      target:"/jsonrpc", // this defines the URL to connect for the services
+      transport:"POST", // We will use POST as the transport
+      envelope:"JSON-RPC-1.0", // We will use JSON-RPC
+      SMDVersion:"2.0",
+      services: {}
+    };
 
-  Rawr.prototype.functions = {};
+    return Rawr;
 
-  Rawr.prototype.smd = {
-    target:"/jsonrpc", // this defines the URL to connect for the services
-    transport:"POST", // We will use POST as the transport
-    envelope:"JSON-RPC-1.0", // We will use JSON-RPC
-    SMDVersion:"2.0",
-    services: {}
-  };
+  });
 
-  return Rawr;
-
-});
+}(
+  typeof define === 'function' && define.amd ? define : function (factory) { module.exports = factory(require); }
+  // Boilerplate for AMD and Node
+));
